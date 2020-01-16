09b61e6e9b67f431476a39515453da687748d56b
Julius Uusinarkaus <julius@uusinarkaus.com>
2020-01-16 12:17:59 +0200

    Parser now parses piped deps

--- modules/parser.js
+++ modules/parser.js
@@ -3,10 +3,15 @@ var fs = require('fs')
 const handle = string => {
   try {
     var data = fs.readFileSync('./status.real', 'utf8')
-    console.log(data)
     const parsed = parse(data)
     const withReverse = reverseDeps(parsed)
-    console.log(withReverse)
+    fs.writeFile('./sample.txt', JSON.stringify(withReverse), err => {
+      if (err) {
+        console.error(err)
+        return
+      }
+      console.log('File has been created')
+    })
   } catch (e) {
     console.log('Error reading file:', e.stack)
   }
@@ -23,7 +28,6 @@ const parse = string => {
     lines.forEach(line => {
       parts = line.split(':')
       if (accepted.includes(parts[0])) p[parts[0]] = parts[1].trim()
-      //
       if (parts[0] === 'Depends') {
         var dependencies = []
         var deps = parts[1].split(',')
@@ -32,6 +36,7 @@ const parse = string => {
         p['Dependencies'] = uniqueDeps
       }
       p['Reverse dependencies'] = []
+      p['Unavailable dependencies'] = []
       if (p['Dependencies'] === undefined) p['Dependencies'] = []
     })
     result[p['Package']] = p
@@ -43,10 +48,29 @@ const parse = string => {
 const reverseDeps = packages => {
   for (const [name, package] of Object.entries(packages)) {
     package['Dependencies'].forEach(dep => {
+      packages = checkForPipes(packages, dep, name)
       if (packages[dep]) packages[dep]['Reverse dependencies'].push(name)
     })
   }
   return packages
 }
 
-handle(sample)
+const checkForPipes = (packages, dependency, name) => {
+  if (dependency.includes('|')) {
+    var options = dependency.split('|')
+    options.forEach(option => {
+      option = option.trim()
+      if (packages[option] !== undefined) {
+        packages[name]['Dependencies'].push(option)
+      } else {
+        packages[name]['Unavailable dependencies'].push(option)
+      }
+    })
+    packages[name]['Dependencies'].splice(packages[name]['Dependencies'].indexOf(dependency), 1 );
+    console.log(packages[name])
+
+  }
+  return packages
+}
+
+handle('sample')
